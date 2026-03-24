05af2d310ca83653f80d6c7474774dcca74f3848
Sigma <sebastenchi@gmail.com>
2026-03-24 20:25:24 -0300

    Se agrega DTFecha completo

--- DTFecha.h
+++ DTFecha.h
@@ -5,4 +5,11 @@ class DTFecha{
         int mes;
         int año;
     public:
+        DTFecha();
+        DTFecha(int dia, int mes, int año);
+        DTFecha(DTFecha &c);
+        int getDia();
+        int getMes();
+        int getAño();
+        ~DTFecha();
 }
